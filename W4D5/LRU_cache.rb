184e5b4ca34d920e3277214d7607382a2f1161b0
misshe <misshenyc@gmail.com>
2020-06-07 13:19:35 -0400

    W4D5 hw cache lru

---
+++ W4D5/LRU_cache.rb
@@ -0,0 +1,66 @@
+  class LRUCache
+
+    attr_accessor :cache, :size
+
+    def initialize(n)
+        @cache = []
+        @size = n
+    end
+
+    def count
+      # returns number of elements currently in cache
+      cache.length
+    end
+
+    def add(el)
+      # adds element to cache according to LRU principle
+      cache.shift if count >= size
+      cache.delete(el) if cache.include?(el)
+      cache << el
+      
+    #   if !cache.include?(el) && count < size 
+    #     cache << el
+    #   elsif !cache.include?(el) && count > size 
+    #     cache << el
+    #     cache.shift
+    #   elsif cache.include?(el) && count < size
+    #     cache.delete(el)
+    #     cache << el
+    #   elsif cache.include?(el) && count > size
+    #     cache.delete(el)
+    #     cache << el
+    #     cache.shift
+    #   end
+    end
+
+    def show
+      # shows the items in the cache, with the LRU item first
+      puts cache
+    end
+
+    private
+    # helper methods go here!
+
+    #find ele in an arr, move ele to the end of the same arr
+
+  end
+
+  johnny_cache = LRUCache.new(4)
+
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(5)
+
+  johnny_cache.count # => returns 2
+
+  johnny_cache.add([1,2,3])
+  johnny_cache.add(5)
+  johnny_cache.add(-5)
+  johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3,4])
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(:ring_of_fire)
+  johnny_cache.add("I walk the line")
+  johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+  johnny_cache.show
